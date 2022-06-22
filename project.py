0712266cbc4a8821896f4401520afb17a3dda6dc
Tom Welsh <4956770+twelsh37@users.noreply.github.com>
2022-06-22 17:24:36 +0100

    Updated project.py created test_project.py

--- project.py
+++ project.py
@@ -20,21 +20,36 @@ Scenario: #Enter scenario name here
 :Created: 21/06/2022
 :Copyright: Tom Welsh - twelsh37@gmail.com
 '''
-
+import os
 import streamlit as st
 import streamlit.components.v1 as components
 import streamlit_authenticator as stauth
 from deta import Deta
 from dotenv import load_dotenv
-import database as db
+#import database as db
 
 
 st.set_page_config(page_title="Data Explorer", layout="wide")
 
+# Load our environment files
+load_dotenv(".env")
+
+# Our Project Keys
+DETA_KEY = os.getenv("DETA_KEY")
+
+# Our Mapbox Token
+MAPBOX_TOKEN = os.getenv("MAPBOX_TOKEN")
+
+# Initialise with a project key
+deta = Deta(DETA_KEY)
+
+# Create database connection
+db = deta.Base("users_db")
+
 # Our Main function
 def main():
     # --- USER AUTHENTICATION ---
-    users = db.fetch_all_users()
+    users = fetch_all_users()
 
     usernames = [user["key"] for user in users]
     names = [user["name"] for user in users]
@@ -69,6 +84,37 @@ def main():
     authenticator.logout("Logout", "sidebar")
 
     st.sidebar.header('Options')
+
+
+# Lets just CRUD it up
+# Create our user
+def create_user(username, name, password):
+    # Returns the user on a successful user creation, otherwise raises an error
+    return db.put({"key": username, "name": name, "password": password})
+
+
+# Read our user info
+def read_user(username):
+    # Returns our user info
+    return db.get(username)
+
+
+# Update our user
+def update_user(updates, username):
+    # Update user details
+    return db.update(updates, username)
+
+
+# Delete our user
+def del_user(username):
+    # Deletes a user
+    return db.delete(username)
+
+
+def fetch_all_users():
+    # returns all our users in the db
+    res = db.fetch()
+    return res.items
 
 if __name__ == '__main__':
     main()
---
+++ test_project.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+'''
+DESCRIPTION:
+Test suitefor project.py
+
+This file tests the fumctions from project.py
+
+:Author: Tom
+:Created: 22/06/2022
+:Copyright: Tom Welsh - twelsh37@gmail.com
+'''
+
+import pytest
+from project import create_user
+from project import read_user
+from project import update_user
+from project import del_user
+from project import fetch_all_users
+
+
+# Catch TypeError
+def test_create_user_typeerror():
+    with pytest.raises(TypeError):
+        create_user(132)
+
+
+# Catch TypeError
+def test_create_user_nameerror():
+    with pytest.raises(NameError):
+        create_user(tom)
+
+
+def test_create_user():
+    assert create_user('atest', 'Test User', 'deleteme')
+
+
+def test_read_user_typeerror():
+    with pytest.raises(TypeError):
+        read_user(132)
+
+
+def test_read_user_nameeerror():
+    with pytest.raises(NameError):
+        read_user(tom)
+
+
+def test_read_user():
+    assert read_user('atest')
+
+##TODO
+# def test_update_user():
+#     if isinstance:
+#         update_user('A Test', atest)
+#     #assert update_user('A real test','atest')
+
+
+def test_del_user_typeerror():
+    with pytest.raises(TypeError):
+        del_user(132)
+
+
+def test_del_user_nameeerror():
+    with pytest.raises(NameError):
+        del_user(tom)
+
+
+def test_del_user():
+    assert del_user('atest') == None
+
+
+def test_fetch_all_users_typeerror():
+    with pytest.raises(TypeError):
+        fetch_all_users(132)
+
+
+def test_fetch_all_users_nameeerror():
+    with pytest.raises(NameError):
+        fetch_all_users(tom)
+
+
+def test_fetch_all_users():
+    assert fetch_all_users()
